c5ef1c669845d2fb3bebe8feb87f41150b6c2233
William O'Connor <woconnor@smu.edu>
2015-03-01 02:09:17 -0600

    Made the charts awesome

--- web/hack.js
+++ web/hack.js
@@ -20,37 +20,50 @@ function removeSignUpButtons(){
 
     function drawChart() {
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');
-      data.addColumn('number', 'Heel Strike');
-      data.addColumn('number', 'Pronation');
+      var run_data = new google.visualization.DataTable();
 
-      data.addRows([
-        [0, 0, 80],   [1, 10, 75],  [2, 23, 72],
-        [3, 17, 70],  [4, 18, 69],  [5, 9, 68],
-        [6, 11, 67],  [7, 27, 65],  [8, 33, 60],
-        [9, 40, 64],  [10, 32, 68], [11, 35, 70],
-        [12, 30, 75], [13, 40, 73], [14, 42, 72],
-        [15, 47, 71], [16, 44, 70], [17, 48, 67],
-        [18, 52, 65], [19, 54, 66], [20, 42, 68],
-        [21, 55, 72], [22, 56, 70], [23, 57, 69],
-        [24, 60, 69], [25, 50, 67], [26, 52, 66],
-        [27, 51, 63], [28, 49, 65], [29, 53, 64],
-        [30, 55, 61], [31, 60, 55], [32, 61, 58],
-        [33, 59, 62], [34, 62, 65], [35, 65, 62],
-        [36, 62, 62], [37, 58, 65], [38, 55, 62],
-        [39, 61, 58], [40, 64, 62], [41, 65, 65],
-        [42, 63, 62], [43, 66, 59], [44, 67, 61],
-        [45, 69, 60], [46, 69, 57], [47, 70, 56],
-        [48, 72, 55], [49, 68, 42], [50, 66, 54],
-        [51, 65, 52], [52, 67, 48], [53, 70, 44],
-        [54, 71, 47], [55, 72, 42], [56, 73, 40],
-        [57, 75, 30], [58, 70, 35], [59, 68, 32],
-        [60, 64, 40], [61, 60, 33], [62, 65, 27],
-        [63, 67, 11], [64, 68, 9], [65, 69, 18],
-        [66, 70, 17], [67, 72, 23], [68, 75, 10],
-        [69, 80, 0]
-      ]);
+      //LAST RUN
+
+      run_data.addColumn('number', 'X');
+      run_data.addColumn('number', 'Heel Strike');
+      run_data.addColumn('number', 'Pronation');
+
+      var data_points = [];
+      var first_point = 50;
+      var second_point = 60;
+      for (var i = 0; i < 300; i++) {
+        var data_point = [];
+        data_point.push(i);
+        first_point -= (Math.random()*5)-(Math.random()*5.1)
+        data_point.push(first_point);
+        second_point -= (Math.random()*4)-(Math.random()*4.1)
+        data_point.push(second_point);
+        data_points.push(data_point);
+      }
+
+      run_data.addRows(data_points);
+
+      //TOTAL RUNS
+
+      var all_data = new google.visualization.DataTable();
+      all_data.addColumn('number', 'X');
+      all_data.addColumn('number', 'Heel Strike');
+      all_data.addColumn('number', 'Pronation');
+
+      data_points = [];
+      first_point = 50;
+      second_point = 60;
+      for (var i = 0; i < 1500; i++) {
+        var data_point = [];
+        data_point.push(i);
+        first_point -= (Math.random()*10)-(Math.random()*10.1)
+        data_point.push(first_point);
+        second_point -= (Math.random()*14)-(Math.random()*14.1)
+        data_point.push(second_point);
+        data_points.push(data_point);
+      }
+
+      all_data.addRows(data_points);
 
       var options = {
         width: 800,
@@ -68,6 +81,6 @@ function removeSignUpButtons(){
       var chart = new google.visualization.LineChart(document.getElementById('ex3'));
       var chart2 = new google.visualization.LineChart(document.getElementById('ex2'));
 
-      chart.draw(data, options);
-      chart2.draw(data, options);
+      chart.draw(run_data, options);
+      chart2.draw(all_data, options);
     }
