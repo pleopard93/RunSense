2922f98f2a8a3872c5123f1ec553d391363c2940
pleopard93 <pleopard93@gmail.com>
2015-03-01 00:08:59 -0600

    Added initial BLE support

--- Run Sense/Run Sense/AppDelegate.m
+++ Run Sense/Run Sense/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [self customizeAppearance];
+    
     return YES;
 }
 
@@ -40,6 +43,11 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void)customizeAppearance
+{
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:kFont size:kFontSizeLarge], NSFontAttributeName, nil]];
 }
 
 @end
--- Run Sense/Run Sense/Definitions.h
+++ Run Sense/Run Sense/Definitions.h
@@ -1,15 +1,11 @@
 //
-//  ViewController.h
+//  Definitions.h
 //  Run Sense
 //
 //  Created by Patrick Leopard on 2/28/15.
 //  Copyright (c) 2015 Hack DFW. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
-
-
-@end
-
+#define kFont @"Raleway"
+#define kFontSize 16
+#define kFontSizeLarge 18
