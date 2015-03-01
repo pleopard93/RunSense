ee4230e3e7e6e17c2ae70bf8a41563a41b54c52b
William O'Connor <woconnor@smu.edu>
2015-03-01 00:18:32 -0600

    google charts

--- web/hack.js
+++ web/hack.js
@@ -15,79 +15,58 @@ function removeSignUpButtons(){
 	$('.sign-up').remove();
 }
 
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+    google.load('visualization', '1', {packages: ['corechart']});
+    google.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Heel Strike');
+      data.addColumn('number', 'Pronation');
+
+      data.addRows([
+        [0, 0, 80],   [1, 10, 75],  [2, 23, 72],
+        [3, 17, 70],  [4, 18, 69],  [5, 9, 68],
+        [6, 11, 67],  [7, 27, 65],  [8, 33, 60],
+        [9, 40, 64],  [10, 32, 68], [11, 35, 70],
+        [12, 30, 75], [13, 40, 73], [14, 42, 72],
+        [15, 47, 71], [16, 44, 70], [17, 48, 67],
+        [18, 52, 65], [19, 54, 66], [20, 42, 68],
+        [21, 55, 72], [22, 56, 70], [23, 57, 69],
+        [24, 60, 69], [25, 50, 67], [26, 52, 66],
+        [27, 51, 63], [28, 49, 65], [29, 53, 64],
+        [30, 55, 61], [31, 60, 55], [32, 61, 58],
+        [33, 59, 62], [34, 62, 65], [35, 65, 62],
+        [36, 62, 62], [37, 58, 65], [38, 55, 62],
+        [39, 61, 58], [40, 64, 62], [41, 65, 65],
+        [42, 63, 62], [43, 66, 59], [44, 67, 61],
+        [45, 69, 60], [46, 69, 57], [47, 70, 56],
+        [48, 72, 55], [49, 68, 42], [50, 66, 54],
+        [51, 65, 52], [52, 67, 48], [53, 70, 44],
+        [54, 71, 47], [55, 72, 42], [56, 73, 40],
+        [57, 75, 30], [58, 70, 35], [59, 68, 32],
+        [60, 64, 40], [61, 60, 33], [62, 65, 27],
+        [63, 67, 11], [64, 68, 9], [65, 69, 18],
+        [66, 70, 17], [67, 72, 23], [68, 75, 10],
+        [69, 80, 0]
+      ]);
+
+      var options = {
+        width: 800,
+        height: 492,
+        hAxis: {
+          title: 'Step'
         },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
+        vAxis: {
+          title: 'Severity'
+        },
+        colors: ['#a52714', '#097138']
+      };
 
-var options = {
+      var chart = new google.visualization.LineChart(document.getElementById('ex3'));
+      var chart2 = new google.visualization.LineChart(document.getElementById('ex2'));
 
-    ///Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : true,
-
-    //String - Colour of the grid lines
-    scaleGridLineColor : "rgba(0,0,0,.05)",
-
-    //Number - Width of the grid lines
-    scaleGridLineWidth : 1,
-
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
-
-    //Boolean - Whether the line is curved between points
-    bezierCurve : true,
-
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension : 0.4,
-
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
-
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 4,
-
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
-
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
-
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
-
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
-
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
-
-    //String - A legend template
-    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-};
-var myLineChart = new Chart(ctx).Line(data, options);
-
+      chart.draw(data, options);
+      chart2.draw(data, options);
+    }
